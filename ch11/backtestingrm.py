bffb39de4d68f7d90e08dbe40bc1264ef8471b2e
E. K. Yi <wolf@teoal.net>
2026-05-17 02:35:46 +0900

    ch11 리스크 관리 백테스트 실습

--- ch11/backtestingrm.py
+++ ch11/backtestingrm.py
@@ -10,18 +10,15 @@ from backtesting import *
 class BacktestingBaseRM(BacktestingBase):
 
     def set_prices(self, price):
-        """Sets prices for tracking of performance.
-        To test for e.g. trailing stop loss hit.
-        """
+        # 진입 가격, 진입 후 최저와 최대 가격 기록...
         self.entry_price = price
         self.min_price = price
         self.max_price = price
 
     def place_buy_order(self, bar, amount=None, units=None, gprice=None):
-        """Places a buy order for a given bar and for
-        a given amount or number of units.
-        """
+        # 매수 주문인데, 원래 클래스와 다른건 gprice와 진입 가격 기록...
         date, price = self.get_date_price(bar)
+        # 이게 뭐냐?
         if gprice is not None:
             price = gprice
         if units is None:
@@ -29,15 +26,14 @@ class BacktestingBaseRM(BacktestingBase):
         self.current_balance -= (1 + self.ptc) * units * price + self.ftc
         self.units += units
         self.trades += 1
+        # 진입 가격 기록...
         self.set_prices(price)
         if self.verbose:
             print(f"{date} | buy {units} units for {price:.4f}")
             self.print_balance(bar)
 
     def place_sell_order(self, bar, amount=None, units=None, gprice=None):
-        """Places a sell order for a given bar and for
-        a given amount or number of units.
-        """
+        # 매도 주문, 원래와 다른건 gprice와 진입 가격 기록...
         date, price = self.get_date_price(bar)
         if gprice is not None:
             price = gprice
